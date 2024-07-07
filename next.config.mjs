e77866fb51d5ea499ff8cc2045f80e08748c7394
Dayanand Gawade <95270686+Daya3611@users.noreply.github.com>
2024-07-07 14:42:19 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,4 +6,37 @@ const nextConfig = {
     }
 };
 
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/(routes)/checkout/page',
+                destination: '/checkout',
+                permanent: true,
+            },
+            {
+                source: '/(routes)/my-order/page',
+                destination: '/my-order',
+                permanent: true,
+            },
+            {
+                source: '/(routes)/order-confermation/page',
+                destination: '/order-confermation',
+                permanent: true,
+            },
+            {
+                source: '/_not-found/page',
+                destination: '/_not-found',
+                permanent: true,
+            },
+            {
+                source: '/page',
+                destination: '/',
+                permanent: true,
+            },
+        ];
+    },
+};
+
+
 export default nextConfig;
