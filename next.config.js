fe46ed8aed9ac62aa7508b249914fb405b295078
Dayanand Gawade <95270686+Daya3611@users.noreply.github.com>
2024-07-07 14:42:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,3 +3,35 @@ module.exports = {
       domains: ['agricart-admin-strapi.onrender.com'],
     },
   }
+
+  module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/(routes)/checkout/page',
+                destination: '/checkout',
+                permanent: true,
+            },
+            {
+                source: '/(routes)/my-order/page',
+                destination: '/my-order',
+                permanent: true,
+            },
+            {
+                source: '/(routes)/order-confermation/page',
+                destination: '/order-confermation',
+                permanent: true,
+            },
+            {
+                source: '/_not-found/page',
+                destination: '/_not-found',
+                permanent: true,
+            },
+            {
+                source: '/page',
+                destination: '/',
+                permanent: true,
+            },
+        ];
+    },
+};
